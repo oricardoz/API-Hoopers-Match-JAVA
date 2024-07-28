9d19898aec087fbd0c3f7a1fa1f9112ac598fdaa
oricardoz <ricardoapzaghetto@gmail.com>
2024-07-27 22:37:33 -0300

    Initial commit

---
+++ src/main/java/br/com/ricardo/HoopersMatch/HoopersMatchApplication.java
@@ -0,0 +1,13 @@
+package br.com.ricardo.HoopersMatch;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class HoopersMatchApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(HoopersMatchApplication.class, args);
+	}
+
+}
---
+++ src/test/java/br/com/ricardo/HoopersMatch/HoopersMatchApplicationTests.java
@@ -0,0 +1,13 @@
+package br.com.ricardo.HoopersMatch;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class HoopersMatchApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
