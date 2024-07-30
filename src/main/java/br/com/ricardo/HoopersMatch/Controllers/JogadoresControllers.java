37a5e68f7e6f484c38a75df31d4bb7969cd92334
oricardoz <ricardoapzaghetto@gmail.com>
2024-07-29 22:27:24 -0300

    refactor: tirando lixo do codigo

--- src/main/java/br/com/ricardo/HoopersMatch/Controllers/JogadoresControllers.java
+++ src/main/java/br/com/ricardo/HoopersMatch/Controllers/JogadoresControllers.java
@@ -1,27 +1,17 @@
 package br.com.ricardo.HoopersMatch.Controllers;
 
-import br.com.ricardo.HoopersMatch.Models.DTO.CadastroJogadorDTO;
-import br.com.ricardo.HoopersMatch.Models.DTO.IdTimeDTO;
-import br.com.ricardo.HoopersMatch.Models.Time;
+
 import br.com.ricardo.HoopersMatch.Repositories.TimeRepository;
 import br.com.ricardo.HoopersMatch.Services.JogadorService;
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.http.HttpStatus;
-import org.springframework.http.ResponseEntity;
 import org.springframework.stereotype.Controller;
-import org.springframework.web.bind.annotation.PostMapping;
-import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.server.ResponseStatusException;
-
-import java.util.Optional;
 
 @Controller
 @RequestMapping("/jogadores")
 public class JogadoresControllers {
 
     private final JogadorService jogadorService;
-    private Optional<Time> time;
 
     @Autowired
     public JogadoresControllers(JogadorService jogadorService, TimeRepository timeRepository) {
--- src/main/java/br/com/ricardo/HoopersMatch/Controllers/TimeController.java
+++ src/main/java/br/com/ricardo/HoopersMatch/Controllers/TimeController.java
@@ -1,15 +1,9 @@
 package br.com.ricardo.HoopersMatch.Controllers;
 
-import br.com.ricardo.HoopersMatch.Models.DTO.CadastroTimeDTO;
 import br.com.ricardo.HoopersMatch.Services.TimeService;
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.http.HttpStatus;
-import org.springframework.http.ResponseEntity;
 import org.springframework.stereotype.Controller;
-import org.springframework.web.bind.annotation.PostMapping;
-import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.server.ResponseStatusException;
 
 @Controller
 @RequestMapping("/time")
