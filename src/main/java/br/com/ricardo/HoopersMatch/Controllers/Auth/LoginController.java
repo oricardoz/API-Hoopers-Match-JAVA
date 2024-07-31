23cd72288261d606f234e65122be2398d8275c83
oricardoz <ricardoapzaghetto@gmail.com>
2024-07-31 18:42:26 -0300

    feat: algumas validacoes no login

--- src/main/java/br/com/ricardo/HoopersMatch/Controllers/Auth/LoginController.java
+++ src/main/java/br/com/ricardo/HoopersMatch/Controllers/Auth/LoginController.java
@@ -8,7 +8,6 @@ import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RequestParam;
 
 @Controller
 @RequestMapping("/")
@@ -16,15 +15,28 @@ public class LoginController {
 
     private final LoginService loginService;
 
+    @Autowired
     public LoginController(LoginService loginService) {
         this.loginService = loginService;
     }
 
     @PostMapping("login")
     public ResponseEntity<String> login(@RequestBody LoginUsuarioDTO loginUsuarioDTO) {
+        String email = loginUsuarioDTO.getLogin();
+        String senha = loginUsuarioDTO.getSenha();
+        if (email == null || email.trim().isEmpty()) {
+            return ResponseEntity.badRequest().body("O email não pode estar em branco");
+        }
+
+        if (!email.contains("@") || !email.contains(".")) {
+            return ResponseEntity.badRequest().body("O email deve ser válido");
+        }
+
+        if (senha == null || senha.trim().isEmpty()) {
+            return ResponseEntity.badRequest().body("A senha não pode estar em branco");
+        }
+
         loginService.autenticarUsuario(loginUsuarioDTO);
         return ResponseEntity.ok().body("Usuario autenticado com sucesso");
     }
-
-
 }
